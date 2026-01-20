a6385facb558057c6f587f0a36cec3f982065b74
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2026-01-20 15:45:08 +0300

    card item modal->dialog

--- src/pages/home/ui/card-item.tsx
+++ src/pages/home/ui/card-item.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { useDisclosure, Modal } from '@heroui/react';
+import { useDisclosure } from '@heroui/react';
 
 import { CardItemDropdown } from './card-item-dropdown';
 import { CardItemModal } from './card-item-modal';
 
 import { Card } from '@/entities/card';
+import { Dialog, DialogContent } from '@/shared/ui/dialog';
 
 interface CardItemProps {
     card: Card;
@@ -18,9 +19,11 @@ export function CardItem({ card }: CardItemProps) {
         <>
             <CardItemDropdown card={card} onModalOpen={onModalOpen} />
 
-            <Modal isOpen={isModalOpen} size="5xl" onClose={onModalClose}>
-                <CardItemModal card={card} onClose={onModalClose} />
-            </Modal>
+            <Dialog open={isModalOpen} onOpenChange={onModalClose}>
+                <DialogContent className="max-w-5xl lg:max-w-5xl" showCloseButton={false}>
+                    <CardItemModal card={card} onClose={onModalClose} />
+                </DialogContent>
+            </Dialog>
         </>
     );
 }
