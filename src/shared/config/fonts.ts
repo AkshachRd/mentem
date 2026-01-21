07e90fe9ab2d897731319132713f0c65b9a4a298
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2026-01-21 10:51:08 +0300

    Local fonts

--- src/shared/config/fonts.ts
+++ src/shared/config/fonts.ts
@@ -1,23 +1,79 @@
-import {
-    IBM_Plex_Mono as FontMono,
-    IBM_Plex_Sans as FontSans,
-    IBM_Plex_Serif as FontSerif,
-} from 'next/font/google';
+import localFont from 'next/font/local';
 
-export const fontSans = FontSans({
-    subsets: ['latin', 'cyrillic'],
-    weight: ['400'],
+export const IBMPlexSans = localFont({
+    src: [
+        {
+            path: '../../../public/IBMPlexSans-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../../../public/IBMPlexSans-Italic.woff2',
+            weight: '400',
+            style: 'italic',
+        },
+        {
+            path: '../../../public/IBMPlexSans-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path: '../../../public/IBMPlexSans-BoldItalic.woff2',
+            weight: '700',
+            style: 'italic',
+        },
+    ],
     variable: '--font-sans',
 });
 
-export const fontMono = FontMono({
-    subsets: ['latin', 'cyrillic'],
-    weight: ['400'],
+export const IBMPlexMono = localFont({
+    src: [
+        {
+            path: '../../../public/IBMPlexMono-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../../../public/IBMPlexMono-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path: '../../../public/IBMPlexMono-BoldItalic.woff2',
+            weight: '700',
+            style: 'italic',
+        },
+        {
+            path: '../../../public/IBMPlexMono-Light.woff2',
+            weight: '300',
+            style: 'normal',
+        },
+    ],
     variable: '--font-mono',
 });
 
-export const fontSerif = FontSerif({
-    subsets: ['latin', 'cyrillic'],
-    weight: ['400'],
+export const IBMPlexSerif = localFont({
+    src: [
+        {
+            path: '../../../public/IBMPlexSerif-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../../../public/IBMPlexSerif-Italic.woff2',
+            weight: '400',
+            style: 'italic',
+        },
+        {
+            path: '../../../public/IBMPlexSerif-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path: '../../../public/IBMPlexSerif-BoldItalic.woff2',
+            weight: '700',
+            style: 'italic',
+        },
+    ],
     variable: '--font-serif',
 });
